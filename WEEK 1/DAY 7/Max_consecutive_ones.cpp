8b6bb0f8f1b11c9ffa9f5cbe6a83bdb050042ecd
Abhijay Krishna <84517361+AbhijayKrishna01@users.noreply.github.com>
2022-08-08 19:08:03 +0530

    Add files via upload

---
+++ WEEK 1/DAY 7/Max_consecutive_ones.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int c=0;
+        int ans=0;
+        // if(nums.size()==0)
+        //     return 0;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(nums[i]!=1)
+        //         continue;
+        //     int j=i+1;
+        //     c=1;
+        //     while(j<nums.size() && nums[j]==1)
+        //     {
+        //         c++;
+        //         j++;
+        //     }
+        //     ans=max(ans,c);
+        //     i=j;
+        // }
+        // return ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==1)
+                c++;
+            else
+                c=0;
+            
+            ans=max(ans,c);
+        }
+        return ans;
+    }
+};
